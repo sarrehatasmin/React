058f8e82237c3a95d7592b28bcfce27fd20449fe
Rikta <tasmin.sarreha@gmail.com>
2022-09-30 23:22:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 console.log("helloo");
-// datatype
+// string literal
 let name="Rikta";
 console.log(typeof name);
 name= '25'
 console.log(typeof name);
+// Object literal
+let info= {
+    'name': 'Rikta',
+    'Bloodgroup': 'A+',
+    'cgpa': '3.93',
+    'age': '23'
+}
+console.log(info);
