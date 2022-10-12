342d51c836c03c26bf009f81132c926a8d8f6ce6
Rikta <tasmin.sarreha@gmail.com>
2022-10-13 04:39:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,18 @@ let info= {
     'age': '23'
 }
 console.log(info);
+//object.property
+let n = info.name;
+console.log(n);
+//object.['property]
+let a= info['age']
+console.log(a);
 info.address='Dhaka'
 console.log(info);
 //dot notation
 console.log(info.bloodGroup);
 //bracket notation
 console.log(info['bloodGroup']);
+info['gender']='female';
+
+console.log(info);
