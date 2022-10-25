7b11c614b2a1be97baf4038456b472bd6833fa4a
Rikta <tasmin.sarreha@gmail.com>
2022-10-25 18:39:42 +0600

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 // create function
-let studentName;
+
 function student (studentName){
     console.log(studentName +" is a brilliant student");
 }
@@ -7,14 +7,10 @@ function student (studentName){
 student("Rikta");
 student("Nitish");
 student("Tasmin");
-console.log(studentName);
-let childName=[];
-let bornPlace =[];
-let time =[];
+
  
 function child (childName,bornPlace,time){
     console.log(childName + " is born in " + bornPlace +"  at " + time);
 }
-console.log(childName, bornPlace,time);
 child(" Sany"," Rajshahi","9 pm");
 child(" Safi"," Mohadevpur","10 pm");
